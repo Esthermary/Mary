1911c06c064e48a423d3dbe2075ea6520a87c0cd
AL NAJEH <emary3583@gmail.com>
2020-02-18 19:00:38 -0800

    Hello Esther Mary

--- app/src/main/java/com/example/esther/MainActivity.java
+++ app/src/main/java/com/example/esther/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -14,14 +17,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button mus=(Button)findViewById(R.id.button);
+        mus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,music.class);
+                startActivity(intent);
+            }
+        });
+
     }
-    public void sendMessage(View view){
-        EditText message=(EditText)findViewById(R.id.message);
-        Intent intent=new Intent(this,DisplayMessageActivity.class);
-        intent.putExtra("MESSAGE",message.getText().toString());
+    public void sendMessage(View view) {
+        EditText message = (EditText) findViewById(R.id.message);
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        intent.putExtra("MESSAGE", message.getText().toString());
         startActivity(intent);
         message.setText("");
-        Toast.makeText (this, "Sending message " + message.getText().toString(),
+        Toast.makeText(this, "Sending message " + message.getText().toString(),
                 Toast.LENGTH_SHORT).show();
     }
-}
+
+        public boolean onCreateOptionsMenu(Menu menu){
+            MenuInflater inflater=getMenuInflater();
+
+           return true;
+        }
+
+    }
+
